d43b7bd0c13c9f22dd03ffbc31a6e4fcd32d2cef
ManojaD2004 <manojadkc2004@gmail.com>
2025-02-12 20:32:28 +0530

    Streams and gRPC done!

--- src/client.ts
+++ src/client.ts
@@ -19,6 +19,14 @@ type HelloResponse = {
 const helloProto = grpc.loadPackageDefinition(packageDefinition)
   .helloworld as grpc.GrpcObject;
 
+function waitForNSeconds(seconds: number) {
+  return new Promise((res, _) => {
+    setTimeout(() => {
+      res(`waited for ${seconds} seconds`);
+    }, 1000 * seconds);
+  });
+}
+
 async function main() {
   const greeter =
     helloProto.Greeter as unknown as grpc.ServiceClientConstructor;
@@ -26,10 +34,59 @@ async function main() {
     "localhost:5051",
     grpc.credentials.createInsecure()
   );
-  client.sayHello({ name: "Manoja" }, function (err: null | Error, response: HelloResponse) {
-    console.log("Greeting:", response.message);
+
+  const a = client.sayHello(
+    { name: "Manoja" },
+    function (err: null | Error, response: HelloResponse) {
+      console.log("Greeting:", response);
+    }
+  );
+  const b = client.sayHelloStreamReply({ name: "Manoja D" });
+  b.on("data", function (msg: any) {
+    console.log(msg);
   });
-  console.log("Hi");
+  b.on("end", function () {
+    console.log("Ended");
+  });
+  b.on("error", function (e: any) {
+    console.log(e);
+  });
+  b.on("status", function (status: any) {
+    console.log(status);
+  });
+  await waitForNSeconds(5);
+  const c = client.sayHelloStreamResponse(function (error: any, stats: any) {
+    if (error) {
+      console.log(error);
+    }
+    console.log("in", stats);
+  });
+  for (let index = 0; index < 5; index++) {
+    await waitForNSeconds(1);
+    console.log(index);
+    c.write({ name: "Manu Warr" });
+  }
+  c.end();
+  const d = client.sayHelloStreamBi();
+  d.on("data", function (msg: any) {
+    console.log(msg);
+  });
+  d.on("end", function () {
+    console.log("Ended");
+  });
+  d.on("error", function (e: any) {
+    console.log(e);
+  });
+  d.on("status", function (status: any) {
+    console.log(status);
+  });
+  for (let index = 0; index < 5; index++) {
+    await waitForNSeconds(1);
+    console.log(index);
+    d.write({ name: "Manu Warr 2004" });
+  }
+  await waitForNSeconds(2);
+  d.end();
 }
 
 main();
--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,8 @@ const packageDefinition = protoLoader.loadSync(protoPath, {
   oneofs: true,
 });
 
-const helloProto = grpc.loadPackageDefinition(packageDefinition).helloworld as grpc.GrpcObject;
+const helloProto = grpc.loadPackageDefinition(packageDefinition)
+  .helloworld as grpc.GrpcObject;
 
 type HelloRequest = {
   name: string;
@@ -20,6 +21,7 @@ type HelloRequest = {
 
 type HelloResponse = {
   message: string;
+  status?: any;
 };
 
 function sayHello(
@@ -27,14 +29,78 @@ function sayHello(
   callback: (err: null | Error, data: HelloResponse) => void
 ) {
   console.log("Hit sayHello");
-  callback(null, { message: `Hello ${call.request.name} welcome to gRPC` });
+  callback(null, {
+    message: `Hello ${call.request.name} welcome to gRPC`,
+  });
+}
+
+function waitForNSeconds(seconds: number) {
+  return new Promise((res, _) => {
+    setTimeout(() => {
+      res(`waited for ${seconds} seconds`);
+    }, 1000 * seconds);
+  });
+}
+
+async function sayHelloStreamReply(call: {
+  request: HelloRequest;
+  write: (a?: any) => void;
+  end: () => void;
+  on: (event: string, fn: () => void) => void;
+}) {
+  for (let index = 0; index < 5; index++) {
+    console.log(index);
+    await waitForNSeconds(1);
+    call.write({ message: `Hello ${call.request.name} welcome to gRPC` });
+  }
+  call.end();
+}
+
+async function sayHelloStreamBi(call: {
+  request: HelloRequest;
+  write: (a?: any) => void;
+  end: () => void;
+  on: (event: string, fn: (a?: any) => void) => void;
+}) {
+  call.on("data", async (a) => {
+    console.log(a);
+    await waitForNSeconds(1);
+    console.log("Sending..");
+    call.write({ message: a.name });
+  });
+  call.on("end", () => {
+    call.end();
+  });
+}
+
+async function sayHelloStreamResponse(
+  call: {
+    request: HelloRequest;
+    on: (event: string, fn: (a?: any) => void) => void;
+  },
+  callback: (err: null | Error, data: any) => void
+) {
+  const b: any[] = [];
+  console.log("In");
+  call.on("data", (a) => {
+    console.log(a);
+    b.push({ message: a.name });
+  });
+  call.on("end", () => {
+    callback(null, { replies: b });
+  });
 }
 
 async function main() {
   const server = new grpc.Server();
   const greeter =
     helloProto.Greeter as unknown as grpc.ServiceClientConstructor;
-  server.addService(greeter.service, { sayHello: sayHello });
+  server.addService(greeter.service, {
+    sayHello: sayHello,
+    sayHelloStreamReply: sayHelloStreamReply,
+    sayHelloStreamResponse: sayHelloStreamResponse,
+    sayHelloStreamBi: sayHelloStreamBi,
+  });
   server.bindAsync(
     "0.0.0.0:5051",
     grpc.ServerCredentials.createInsecure(),
